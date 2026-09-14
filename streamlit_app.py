810c7c280b7a768ca93142109f32cb4ba9bdf2b2
rbucs <rbucs@me.com>
2026-09-13 20:37:54 -0500

    Enhance fruit options selection and display
    Updated the data selection to include 'SEARCH_ON' and added a dataframe display for the selected fruit options. Removed unnecessary code related to a specific fruit request.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ name_on_order = st.text_input("Name on Smoothie:")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -29,6 +30,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -43,6 +47,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
